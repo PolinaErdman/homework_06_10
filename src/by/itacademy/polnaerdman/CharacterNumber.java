4f3217d7c62eb152e1baed0749fa11c590e6e0e6
palinaerdman <polinaerdman@gmail.com>
2024-10-19 15:07:54 +0300

    add class CharacterNumber

---
+++ src/by/itacademy/polnaerdman/CharacterNumber.java
@@ -0,0 +1,26 @@
+package by.itacademy.polnaerdman;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CharacterNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter text: ");
+        String text = scanner.nextLine();
+        scanner.close();
+
+        HashMap<Character, Integer> charMap = new HashMap<>();
+        for(char ch : text.toCharArray()) {
+            if (charMap.containsKey(ch))
+                charMap.replace(Character.valueOf(ch), charMap.get(ch) + 1);
+            else
+                charMap.put(Character.valueOf(ch), 1);
+        }
+
+        for (Map.Entry<Character, Integer> entry : charMap.entrySet()) {
+            System.out.println(entry.getKey() + " count is " + entry.getValue());
+        }
+    }
+}
